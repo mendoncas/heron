44189778b6a77d16f258a80f27d0303e383d0635
Ricardo César <ricardocesarfilho@unifor.br>
2024-04-09 13:26:39 -0300

    infra

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Pull based deployer!
+# Pull-based deployer!
 This project aims to mimic the behavior of pull-based deployers like ArgoCD. 
 The intended goal is to have a small program that, given a certain git repository, provisions minimal infrastructure to deploy the project 
 and constantly watches over the repo, identifying and applying changes.
 
-Needless to day, this is a experimental project that im using for practice.
+Needless to day, this is an experimental project that im using for practice.
--- deployer/main.sh
+++ deployer/main.sh
@@ -1,21 +1,22 @@
-#!/bin/env sh
+#!/bin/bash
 #
 GIT_REPO=$1
 IMAGE_NAME=$2
 DOWNLOAD_PATH=$3
 
 git clone $GIT_REPO $DOWNLOAD_PATH
-docker build -t $IMAGE_NAME:heron ./$DOWNLOAD_PATH
+docker build -t $IMAGE_NAME:heron $DOWNLOAD_PATH
 nohup docker run -p 8080:80 $IMAGE_NAME:heron & #run detached process 
 
 cd $DOWNLOAD_PATH
 while true; do 
   sleep 10
   echo "pulling!"
-  pull=$(git pull)
-  if [[ $pull != 'Already up to date.' ]]; then
+  pull="$(git pull)"
+  pwd
+  echo $pull
+  if [[ "$pull" != 'Already up to date.' ]]; then
     echo "change detected"
-    echo $pull
     docker stop $(docker ps -q)
     docker build -t $IMAGE_NAME:heron .
     nohup docker run -p 8080:80 $IMAGE_NAME:heron & #run detached process 
